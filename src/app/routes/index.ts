c72620780aa0c93c0c3ca5dbb3d1dc628b363a54
Mehedi Hassan <rehan51322@gmail.com>
2023-06-16 13:02:33 +0600

    deploy bug fix again

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { academicDepartmentRoutes } from "../modules/academicDepartment/academicDepartment.route";
+import { AcademicDepartmentRoutes } from "../modules/academicDepartment/academicDepartment.route";
 import { AcademicFacultyRoutes } from "../modules/academicFaculty/academicFaculty.route";
 import { AcademicSemesterRoutes } from "../modules/academicSemester/academicSemester.route";
 import { StudentRoutes } from "../modules/student/student.route";
@@ -23,7 +23,7 @@ const moduleRoutes = [
   },
   {
     path: "/academic-departments",
-    route: academicDepartmentRoutes,
+    route: AcademicDepartmentRoutes,
   },
   {
     path: "/students",
